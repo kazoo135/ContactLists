f27fe010bac1550a9737e6bc8a224fa51f7e717a
Mark Caruso <kazoo6723@gmail.com>
2017-05-20 05:54:23 -0400

    Added all files required for contact-item, added contact-item selector to app.commponent.html

---
+++ src/app/contact-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-item',
+  templateUrl: './partials/contact-item.component.html',
+  styleUrls: ['./css/contact-item.component.css']
+})
+
+export class ContactItemComponent {
+  
+}
