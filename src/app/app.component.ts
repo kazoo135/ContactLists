443a660946a540b957a7f526b0773173969d485a
Mark Caruso <kazoo6723@gmail.com>
2017-05-30 19:57:46 -0400

    Setup ContactList project with all starting files. This includes app.component and an initial contact-item component. Created folders for partial html and css files and added new files to each folder. The project is now under way.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'cl-app',
+  templateUrl: 'app/partials/cl-app.component.html',
+  styleUrls: ['app/css/cl-app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+}
---
+++ src/app/cl-contact-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: '<cl-contact-item></cl-contact-item>',
+  templateUrl: 'app/partials/cl-contact-item.component.ts',
+  styleUrls: ['app/css/cl-contact-item.component.css']
+})
+
+export class ContactItemComponent {
+
+}
