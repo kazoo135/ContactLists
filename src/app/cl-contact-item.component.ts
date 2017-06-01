fb2cf20b85a601b9b44bca818a4be4c0aff81194
Mark Caruso <kazoo6723@gmail.com>
2017-06-01 11:16:00 -0400

    Added the Input and Ouput Decorators to contactItemComponent and moved my contactItem obj to the app.component file. Wired up the onDelte() and onContactItemDelete() methods.

--- src/app/cl-contact-item.component.ts
+++ src/app/cl-contact-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 
 @Component({
@@ -8,35 +8,13 @@ import { Component } from '@angular/core';
 })
 
 export class ContactItemComponent {
+  @Input() contactItem: any;
+@Output() delete = new EventEmitter();
 
-  contactItem = {
-    firstname: 'Ben',
-    lastname: 'Sargent',
-    relationship: 'friend',
-    birthday: 'January 9, 1955',
-    address: {
-      home: {
-        street: '238 whitehall Rd',
-        city: 'Denver',
-        state: 'Colorado',
-        zipcode: '100019'
-      },
-      work: {
-        street: '838 Maple Drive',
-        city: 'Denver',
-        state: 'Colorado',
-        zipcode: '100987',
-      },
-    },
-    phone: {
-        home: '393-899-1234',
-        mobile: '837-999-7373',
-        work: '837-182-8888'
-      },
-      email: {
-        primary: 'bsargent@gmail.com',
-        secondary: 'ben@example.com'
-      }
-  };
+onDelete() {
+    console.log('Item Deleted');
+  // this will return the contactItem when the delete button is clicked
+    this.delete.emit(this.contactItem);
+  }
 
 }
