2dae80b8ccad19bd5035918dd395c8508705021d
Mark Caruso <kazoo6723@gmail.com>
2017-06-05 19:12:29 -0400

    Added contact-item-form.component.ts along with other necessary files. Added form component to app.mofule. Built the form.html and am looking to fill in some select options using ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ContactItemComponent } from './cl-contact-item.component';
 import { ContactItemListComponent} from './cl-contact-item-list.component';
-
+import { ContactItemFormComponent } from './cl-contact-item-form.component';
 @NgModule({
   imports: [BrowserModule],
   declarations: [AppComponent,
     ContactItemComponent,
-  ContactItemListComponent],
+  ContactItemListComponent,
+  ContactItemFormComponent],
   bootstrap: [AppComponent]
 })
 
---
+++ src/app/cl-contact-item-form.component.ts
@@ -0,0 +1,55 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'cl-contact-item-form',
+  templateUrl: 'app/partials/cl-contact-item-form.component.html',
+  styleUrls: ['app/css/cl-contact-item-form.component.css']
+})
+
+export class ContactItemFormComponent {
+  months: any[];
+  days: any;
+  years: any;
+  states = [{
+    name: 'Alabama',
+    abbr: 'AL'
+  },
+    {
+      name: 'Alaska',
+      abbr: 'AK'
+    },
+    {
+      name: 'Arizona',
+      abbr: 'AZ'
+    },
+    {
+      name: 'Arkansas',
+      abbr: 'AR'
+    },
+    {
+      name: 'California',
+      abbr: 'CA'
+    }];
+
+  initDays() {
+    for ( let i = 1; i < 32; i++) {
+      this.days[i] = i;
+    }
+  }
+
+  initYears() {
+    for ( let i = 2000; i < 2018; i++) {
+      this.years[i] = i;
+    }
+    console.log(this.years);
+  }
+  ngOnInit() {
+    this.months = [ 'January', 'Feburary', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+    // TODO eventually I want to figure out how to initialize days and years with loops
+    this.days = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    // this.years = ['2001', '2002', '2003', '2004', '2006'];
+    // this.initDays();
+    // this.initYears();
+  }
+} // End of ContactItemFormComponent class
